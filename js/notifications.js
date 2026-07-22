891d39dc11c3c8b073e95ffb4c0e308fdd6d4a01
abdulihad <kausar210jahan@gmail.com>
2026-07-22 19:38:46 +0545

    Fix FCM token registration timing

--- js/notifications.js
+++ js/notifications.js
@@ -1,44 +1,72 @@
 import { auth, db } from "./firebase.js";
 
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging.js";
+import {
+    getMessaging,
+    getToken
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging.js";
 
-import { doc, updateDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import {
+    doc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
 
 const messaging = getMessaging();
 
+
 export async function registerNotifications() {
 
-    const permission = await Notification.requestPermission();
+    onAuthStateChanged(auth, async (user) => {
 
-    if (permission !== "granted") {
+        if (!user) {
+            console.log("No user logged in yet.");
+            return;
+        }
 
-        console.log("Notification permission denied.");
 
-        return;
+        const permission = await Notification.requestPermission();
 
-    }
 
-    const token = await getToken(messaging, {
+        if (permission !== "granted") {
 
-        vapidKey: "BGzmph-FX3MzgIZN-S2WzyaXLxr-bZhdOSUjLJSTO9OMn6WxUQjm-_YsJkcdNStMWgOF2N4cVShw4KJwYbdFUTY"
+            console.log("Notification permission denied.");
+
+            return;
+
+        }
+
+
+        const token = await getToken(messaging, {
+
+            vapidKey: "BGzmph-FX3MzgIZN-S2WzyaXLxr-bZhdOSUjLJSTO9OMn6WxUQjm-_YsJkcdNStMWgOF2N4cVShw4KJwYbdFUTY"
+
+        });
+
+
+        if (!token) {
+
+            console.log("No FCM token.");
+
+            return;
+
+        }
+
+
+        console.log("FCM Token:", token);
+
+
+        await updateDoc(
+            doc(db, "users", user.uid),
+            {
+                fcmToken: token
+            }
+        );
+
 
     });
-
-    if (!token) {
-
-        console.log("No FCM token.");
-
-        return;
-
-    }
-
-    console.log("FCM Token:", token);
-
-    await updateDoc(
-        doc(db, "users", auth.currentUser.uid),
-        {
-            fcmToken: token
-        }
-    );
 
 }
