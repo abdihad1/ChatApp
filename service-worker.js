5f591c786cda9422448863c47938683d6ebe470e
abdulihad <kausar210jahan@gmail.com>
2026-07-30 10:00:50 +0545

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chativo-v5";
+const CACHE_NAME = "chativo-v6";
 
 const FILES_TO_CACHE = [
     "/",
@@ -6,6 +6,7 @@ const FILES_TO_CACHE = [
     "/login.html",
     "/signup.html",
     "/css/style.css",
+    "/css/modern.css",
     "/manifest.json"
 ];
 
